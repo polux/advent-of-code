9b5884b330c4c00628901ed5be388b48fee079e7
Paul Brauner <polux@google.com>
2022-11-15 13:05:41 +0100

    2021 day 1-2 in racket

---
+++ 2021/day1-2/src/main.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+(require advent-of-code)
+(require threading)
+(require seq)
+(require relation/composition)
+
+(define str-input
+  (fetch-aoc-input (find-session) 2021 1 #:cache #t))
+
+(define (parse str)
+  (~>> str
+       string-split
+       (map string->number)))
+
+(define (solve input)
+  (define (delta x y)
+    (if (> y x) 1 0))
+  (~>> (zip-with delta input (drop 3 input))
+       sum))
+
+(define (pretty sol) sol)
+
+(~>> str-input
+     parse
+     solve
+     pretty)
