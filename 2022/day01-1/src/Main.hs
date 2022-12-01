03cd550f9d96374c624e659af576cf001a2f1a81
Paul Brauner <polux@google.com>
2022-12-01 13:48:30 +0100

    2022 day 1

---
+++ 2022/day01-1/src/Main.hs
@@ -0,0 +1,21 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+
+module Main where
+
+import Data.List (sortBy)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Ord (Down (..), comparing)
+
+type Input = [[Int]]
+
+type Output = Int
+
+main :: IO ()
+main = readFile "input" >>= print . solve . parse
+
+parse :: String -> Input
+parse = map (map read) . splitOn [""] . lines
+
+solve :: Input -> Output
+solve = maximum . map sum
