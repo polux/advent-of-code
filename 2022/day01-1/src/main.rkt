bb576fb75dbd5f49f580d89766d19844b6f933aa
Paul Brauner <polux@google.com>
2022-12-06 09:13:23 +0100

    2022 day1-1 racket

---
+++ 2022/day01-1/src/main.rkt
@@ -0,0 +1,25 @@
+#lang curly-fn racket
+
+(require advent-of-code)
+(require threading)
+
+(define str-input
+  (fetch-aoc-input (find-session) 2022 1 #:cache #t))
+
+(define (parse str)
+  (~>> str
+       (string-split _ "\n\n")
+       (map (lambda~>> string-split
+                       (map string->number)))))
+
+(define (solve input)
+  (~>> input
+       (map #{apply + %})
+       (apply max)))
+
+(define (pretty sol) sol)
+
+(~>> str-input
+     parse
+     solve
+     pretty)
