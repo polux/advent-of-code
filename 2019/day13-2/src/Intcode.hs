75b7cf09843eafc578b2e8ac44db5f35489e7e9f
Paul Brauner <polux@google.com>
2023-01-19 11:09:57 +0100

    Better undo in 2019 day 13

--- 2019/day13-2/src/Intcode.hs
+++ 2019/day13-2/src/Intcode.hs
@@ -1,35 +1,35 @@
+{-# LANGUAGE FlexibleContexts #-}
 -- Copyright 2021 Google LLC.
 -- SPDX-License-Identifier: Apache-2.0
-
 {-# OPTIONS -Wall #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE NamedFieldPuns #-}
 
 module Intcode where
 
 import Control.Monad.Reader
 import Data.Function
+import Data.Kind (Type)
 import Data.List (sortOn)
 import Data.List.Split (splitOn)
-import Data.Vector ((!), (//))
-import Graphics.Gloss.Interface.Pure.Game
-import qualified Data.Vector as V
 import qualified Data.Map as M
+import Data.Vector ((!), (//))
+import qualified Data.Vector as V
 import Debug.Trace (traceShow, traceShowId)
+import Graphics.Gloss.Interface.Pure.Game
 
 type Vec = V.Vector Integer
 
 parseInstruction :: Integer -> (Integer, Int, Int, Int)
 parseInstruction i =
-    ( i `rem` 100
-    , fromIntegral  ((i `div` 100) `mod` 10)
-    , fromIntegral  ((i `div` 1000) `mod` 10)
-    , fromIntegral  ((i `div` 10000) `mod` 10)
-    )
+  ( i `rem` 100
+  , fromIntegral ((i `div` 100) `mod` 10)
+  , fromIntegral ((i `div` 1000) `mod` 10)
+  , fromIntegral ((i `div` 10000) `mod` 10)
+  )
 
-data Run :: * -> * where
+data Run :: Type -> Type where
   Done :: a -> Run a
   ReadInput :: (Integer -> Run a) -> Run a
   WriteOutput :: Integer -> Run a -> Run a
@@ -43,11 +43,10 @@ instance Functor Run where
   fmap = liftM
 
 instance Applicative Run where
-  pure = return
+  pure = Done
   (<*>) = ap
 
 instance Monad Run where
-  return = Done
   (Done x) >>= f = f x
   (ReadInput k) >>= f = ReadInput (k >=> f)
   (WriteOutput n k) >>= f = WriteOutput n (k >>= f)
@@ -63,49 +62,39 @@ type Machine = Run ()
 run :: Integer -> Integer -> Vec -> Machine
 run b i vec
   | op == 99 = return ()
-  | op == 1 = run b (i+4) (vec // [(fromIntegral imm3, v1 + v2)])
-  | op == 2 = run b (i+4) (vec // [(fromIntegral imm3, v1 * v2)])
+  | op == 1 = run b (i + 4) (vec // [(fromIntegral i3', v1 + v2)])
+  | op == 2 = run b (i + 4) (vec // [(fromIntegral i3', v1 * v2)])
   | op == 3 = do
-     input <- readInput
-     run b (i+2) (vec // [(fromIntegral imm1, input)])
+      input <- readInput
+      run b (i + 2) (vec // [(fromIntegral i1, input)])
   | op == 4 = do
-     writeOutput v1
-     run b (i+2) vec
-  | op == 5 = run b (if v1 /= 0 then v2 else i+3) vec
-  | op == 6 = run b (if v1 == 0 then v2 else i+3) vec
-  | op == 7 = run b (i+4) (vec // [(fromIntegral imm3, if v1 < v2 then 1 else 0)])
-  | op == 8 = run b (i+4) (vec // [(fromIntegral imm3, if v1 == v2 then 1 else 0)])
-  | op == 9 = run (b+v1) (i+2) vec
+      writeOutput v1
+      run b (i + 2) vec
+  | op == 5 = run b (if v1 /= 0 then v2 else i + 3) vec
+  | op == 6 = run b (if v1 == 0 then v2 else i + 3) vec
+  | op == 7 = run b (i + 4) (vec // [(fromIntegral i3', if v1 < v2 then 1 else 0)])
+  | op == 8 = run b (i + 4) (vec // [(fromIntegral i3', if v1 == v2 then 1 else 0)])
+  | op == 9 = run (b + v1) (i + 2) vec
   | otherwise = error ("unknown op code " ++ show op)
  where
-  (op, p1, p2, p3) = parseInstruction i0
-  v1 | p1 == 1 = i1
-     | p1 == 2 = r1 b
-     | otherwise = r1 0
-  v2 | p2 == 1 = i2
-     | p2 == 2 = r2 b
-     | otherwise = r2 0
-  v3 | p3 == 1 = i3
-     | p3 == 2 = r3 b
-     | otherwise = r3 0
-  i0 = vec ! fromIntegral i
-  i1 = vec ! fromIntegral (i+1)
-  i2 = vec ! fromIntegral (i+2)
-  i3 = vec ! fromIntegral (i+3)
-  r1 o = vec ! fromIntegral (i1+o)
-  r2 o = vec ! fromIntegral (i2+o)
-  r3 o = vec ! fromIntegral (i3+o)
-  imm1 | p1 == 0 = i1
-       | p1 == 2 = i1 + b
-  imm2 | p2 == 0 = i2
-       | p2 == 2 = i2 + b
-  imm3 | p3 == 0 = i3
-       | p3 == 2 = i3 + b
+  (op, p1, p2, p3) = parseInstruction (vec ! fromIntegral i)
+  i1 = vec ! fromIntegral (i + 1)
+  i2 = vec ! fromIntegral (i + 2)
+  i3 = vec ! fromIntegral (i + 3)
+  i1' = i1 + if p1 == 0 then 0 else b
+  i2' = i2 + if p2 == 0 then 0 else b
+  i3' = i3 + if p3 == 0 then 0 else b
+  v1
+    | p1 == 1 = i1
+    | otherwise = vec ! fromIntegral i1'
+  v2
+    | p2 == 1 = i2
+    | otherwise = vec ! fromIntegral i2'
 
 evalList :: [Integer] -> Run a -> [Integer]
-evalList is (Done x) = []
+evalList _ (Done _) = []
 evalList [] (ReadInput _) = error "no input to read from"
-evalList (i:is) (ReadInput k) = evalList is (k i)
+evalList (i : is) (ReadInput k) = evalList is (k i)
 evalList is (WriteOutput o k) = o : evalList is k
 
 execute :: [Integer] -> Vec -> [Integer]
@@ -115,7 +104,7 @@ type Pos = (Integer, Integer)
 type Screen = M.Map Pos Integer
 
 toScreen :: [Integer] -> Screen
-toScreen (x:y:t:os) = M.insert (x,y) t (toScreen os)
+toScreen (x : y : t : os) = M.insert (x, y) t (toScreen os)
 toScreen _ = M.empty
 
 parse :: String -> Vec
@@ -124,42 +113,71 @@ parse s = s & splitOn "," & map read & V.fromList
 parseAndExecute :: String -> [Integer] -> [Integer]
 parseAndExecute s input = parse s & execute input
 
-data GameState = GameState
+data GameState = G
   { screen :: Screen
   , machine :: Machine
   , score :: Integer
+  }
+
+data AppState = A
+  { gameState :: GameState
   , history :: [GameState]
   , auto :: Bool
   }
 
-update :: GameState -> GameState
-update s@GameState {machine = ReadInput _, auto = True} = handle (EventKey (SpecialKey KeySpace) Down undefined undefined) s
-update s@GameState {screen, machine=WriteOutput x (WriteOutput y (WriteOutput t machine'))}
-  | x == -1 && y == 0 = s { score = t, machine = machine' }
-  | otherwise  = update (s { screen = M.insert (x,y) t screen, machine = machine' })
+update :: AppState -> AppState
+update s@A{gameState = G{machine = ReadInput _}, auto = True} =
+  handle (EventKey (SpecialKey KeySpace) Down undefined undefined) s
+update s@A{gameState = g@G{screen, machine = WriteOutput x (WriteOutput y (WriteOutput t machine'))}}
+  | x == -1 && y == 0 = s{gameState = g{score = t, machine = machine'}}
+  | otherwise = update (s{gameState = g{screen = M.insert (x, y) t screen, machine = machine'}})
 update s = s
 
-handle :: Event -> GameState -> GameState
-handle (EventKey (SpecialKey KeyLeft) Down _ _) s@GameState{machine = ReadInput k, history} = s { machine = k (-1), history = s:history }
-handle (EventKey (SpecialKey KeyRight) Down _ _) s@GameState{machine = ReadInput k, history} = s { machine = k 1, history = s:history }
-handle (EventKey (SpecialKey KeyUp) Down _ _) s@GameState{machine = ReadInput k, history} = s { machine = k 0, history = s:history }
-handle (EventKey (Char 'z') Down _ _) GameState{history =(s:_)} = s
-handle (EventKey (Char 'a') Down _ _) s@GameState{auto} = s { auto = not auto }
-handle (EventKey (SpecialKey KeySpace) Down _ _) s@GameState{screen, machine = ReadInput k, history} = s { machine = k sign, history = s:history }
- where sign | ballX < paddleX = -1
-            | ballX > paddleX = 1
-            | otherwise = 0
-       ballX = M.filter (==4) screen & M.keys & head
-       paddleX = M.filter (==3) screen & M.keys & head
+handle :: Event -> AppState -> AppState
+handle
+  (EventKey (SpecialKey KeyLeft) Down _ _)
+  s@A{gameState = g@G{machine = ReadInput k}, history} =
+    s{gameState = g{machine = k (-1)}, history = g : history}
+handle
+  (EventKey (SpecialKey KeyRight) Down _ _)
+  s@A{gameState = g@G{machine = ReadInput k}, history} =
+    s{gameState = g{machine = k 1}, history = g : history}
+handle
+  (EventKey (SpecialKey KeyUp) Down _ _)
+  s@A{gameState = g@G{machine = ReadInput k}, history} =
+    s{gameState = g{machine = k 0}, history = g : history}
+handle
+  (EventKey (Char 'z') Down _ _)
+  s@A{history = g : gs} =
+    s{gameState = g, history = gs}
+handle
+  (EventKey (Char 'a') Down _ _)
+  s@A{auto} =
+    s{auto = not auto}
+handle
+  (EventKey (SpecialKey KeySpace) Down _ _)
+  s@A{gameState = g@G{screen, machine = ReadInput k}, history} =
+    s{gameState = g{machine = k sign}, history = g : history}
+   where
+    sign
+      | ballX < paddleX = -1
+      | ballX > paddleX = 1
+      | otherwise = 0
+    ballX = M.filter (== 4) screen & M.keys & head
+    paddleX = M.filter (== 3) screen & M.keys & head
 handle _ s = s
 
-render :: GameState -> Picture
-render GameState{screen, score} = translate (-500) 300 (color white (text (show score))) <> scale 10 (-10) (mconcat (map tile (sortOn snd (M.toList screen))))
+render :: AppState -> Picture
+render A{gameState = G{screen, score}} =
+  mconcat
+    [ translate 0 20 (scale 0.2 0.2 (color white (text (show score))))
+    , scale 10 (-10) (mconcat (map tile (sortOn snd (M.toList screen))))
+    ]
  where
-  tile ((x,y), 3) = color red (translate (fromIntegral x) (fromIntegral y) (rectangleSolid 3 1))
-  tile ((x,y), t) = color (colorOf t) (translate (fromIntegral x) (fromIntegral y) (rectangleSolid 1 1))
+  tile ((x, y), t) = color (colorOf t) (translate (fromIntegral x) (fromIntegral y) (rectangleSolid 1 1))
   colorOf 1 = white
   colorOf 2 = blue
+  colorOf 3 = red
   colorOf 4 = green
   colorOf _ = black
 
@@ -169,10 +187,10 @@ game vec =
     FullScreen
     black
     200
-    initialGameState
+    initialAppState
     render
     handle
     (const update)
  where
-  initialGameState = GameState { screen = M.empty, machine = initialMachine, score = 0, history = [], auto = False }
-  initialMachine = run 0 0 ((vec // [(0, 2)]) <> V.replicate 500 0)
+  initialAppState = A{gameState = G{screen = M.empty, machine = initialMachine, score = 0}, history = [], auto = False}
+  initialMachine = run 0 0 ((vec // [(0, 2)]) <> V.replicate 20 0)
