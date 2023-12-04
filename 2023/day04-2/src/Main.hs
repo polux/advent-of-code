1ba004f4bc980f500a9ef3b836cd32d6d744bd56
Paul Brauner <polux2001@gmail.com>
2023-12-04 07:40:42 +0100

    2023 day 4

---
+++ 2023/day04-2/src/Main.hs
@@ -0,0 +1,112 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+-- #region language extensions
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+{-# HLINT ignore "Eta reduce" #-}
+
+-- #endregion
+
+module Main where
+
+-- #region imports
+
+import Control.Arrow (Arrow (first, second), (***), (>>>))
+import Control.Lens (at, each, folded, isn't, ix, traversed, (%~), (&), (.~), (?~), (^.), (^..), (^?), _1, _2, _3, _4)
+import Control.Lens.Extras (is)
+import Control.Monad (forM_, unless, when)
+import qualified Data.Array as A
+import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import Data.Functor ((<&>))
+import Data.Generics.Labels ()
+import qualified Data.Graph.Inductive as G
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.List (elemIndex, sortOn)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes, fromJust)
+import Data.MemoTrie
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
+import Debug.Trace (trace, traceShow, traceShowId)
+import GHC.Generics (Generic)
+import Linear (V2 (..), _x, _y)
+import Safe hiding (at)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Pretty.Simple (pPrint, pShow)
+import Text.Regex.PCRE ((=~))
+import Util
+
+-- #endregion
+
+-- #region regex parsing
+{-
+import Text.Regex.Pcre2 (regex)
+
+parseRegex :: String -> [Int]
+parseRegex = map parseLine . T.lines . T.pack
+ where
+   toInt = read . T.unpack
+   parseLine [regex|some example regex: (?<x>\d+)|] = toInt x
+-}
+-- #endregion
+
+type Input = [([Int], [Int])]
+
+-- type Output = Input
+
+main :: IO ()
+main = readFile "input" >>= print . solve . parse
+
+parse :: String -> Input
+parse = map parseLine . lines
+ where
+  parseLine (splitOn ":" -> [_, splitOn "|" -> [numsLeft, numsRight]]) =
+    (parseNums *** parseNums) (numsLeft, numsRight)
+  parseNums = map read . words
+
+-- solve :: Input -> Output
+solve input = sum $ go (map numMatches input) (repeat 1)
+ where
+  numMatches (left, right) =
+    let sleft = S.fromList left
+     in length [n | n <- right, n `S.member` sleft]
+  plus xs ys = zipWith (+) (xs ++ repeat 0) ys
+  go [] _ = []
+  go (n : ns) (m : ms) = m : go ns (plus (replicate n m) ms)
+
+{-
+
+4,2,2,1,0,0
+1,1,1,1,1,1
+1,2,2,2,1,1
+1,2,4,4,1,1
+1,2,4,8,1,1
+
+-}
