c9faae9d796c092bfa28ba5c5052ce31ccd07735
Paul Brauner <polux2001@gmail.com>
2024-12-10 06:12:32 +0100

    2024 day 10

---
+++ 2024/day10-1/src/Main.hs
@@ -0,0 +1,96 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+
+-- #region language extensions
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE QuasiQuotes #-}
+-- #endregion
+
+module Main where
+
+-- #region imports
+
+import Control.Arrow ((>>>),(***), Arrow (first, second))
+import Control.Lens (at, ix, each, folded, isn't, traversed, (%~), (&), (.~), (?~), (^.), (^..), (^?), _1, _2, _3, _4)
+import Control.Lens.Extras (is)
+import qualified Data.Array as A
+import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import Data.Generics.Labels ()
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
+import Debug.Trace (trace, traceShow, traceShowId)
+import GHC.Generics (Generic)
+import Linear (V2 (..), _x, _y)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Pretty.Simple (pPrint, pShow)
+import Text.Regex.PCRE ((=~))
+import Util
+import qualified Data.Graph.Inductive as G
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Safe hiding (at)
+import Data.Functor ((<&>))
+import Data.MemoTrie
+import Data.Maybe (fromJust, catMaybes)
+import Data.List (sortOn, elemIndex)
+import Data.List.Split (splitOn, chunksOf)
+import Control.Monad (forM_, unless, when)
+
+-- #endregion
+
+-- #region regex parsing
+{-
+import Text.Regex.Pcre2 (regex)
+
+parseRegex :: String -> [Int]
+parseRegex = map parseLine . T.lines . T.pack
+ where
+   toInt = read . T.unpack
+   parseLine [regex|some example regex: (?<x>\d+)|] = toInt x
+-}
+-- #endregion
+
+type Input = UA.Array (V2 Int) Int
+
+
+main :: IO ()
+main = readFile "input" >>= print . solve . parse
+
+parse :: String -> Input
+parse = arrayFromList2D . map (map (read . singleton)) . lines
+
+solve input = sum $ map score zeroes
+  where
+    at p = input UA.! p
+    zeroes = [p | p <- UA.indices input, at p == 0]
+    size = arraySize input
+
+    score p = length (reachableNines p 0)
+
+    mReachableNines = memo2 reachableNines
+
+    reachableNines p 9 = S.singleton p
+    reachableNines p n = S.unions [mReachableNines pn (n+1) | pn <- neighbors2D size p, at pn == (n+1)]
