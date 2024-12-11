74c4f5446f28f8889aaad5453b43adc873ce61b4
Paul Brauner <polux2001@gmail.com>
2024-12-11 06:26:35 +0100

    2024 day 11

---
+++ 2024/day11-1/src/Main.hs
@@ -0,0 +1,106 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+-- #region language extensions
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+
+-- #endregion
+
+module Main where
+
+-- #region imports
+
+import Control.Arrow (Arrow (first, second), (***), (>>>))
+import Control.Lens (at, each, folded, isn't, ix, traversed, (%~), (&), (.~), (?~), (^.), (^..), (^?), _1, _2, _3, _4)
+import Control.Lens.Extras (is)
+import Control.Monad (forM_, unless, when)
+import qualified Data.Array as A
+import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import Data.Functor ((<&>))
+import Data.Generics.Labels ()
+import qualified Data.Graph.Inductive as G
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.List (elemIndex, sortOn)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes, fromJust)
+import Data.MemoTrie
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
+import Debug.Trace (trace, traceShow, traceShowId)
+import GHC.Generics (Generic)
+import Linear (V2 (..), _x, _y)
+import Safe hiding (at)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Pretty.Simple (pPrint, pShow)
+import Text.Regex.PCRE ((=~))
+import Util
+
+-- #endregion
+
+-- #region regex parsing
+{-
+import Text.Regex.Pcre2 (regex)
+
+parseRegex :: String -> [Int]
+parseRegex = map parseLine . T.lines . T.pack
+ where
+   toInt = read . T.unpack
+   parseLine [regex|some example regex: (?<x>\d+)|] = toInt x
+-}
+-- #endregion
+
+type Input = [Int]
+
+main :: IO ()
+main = readFile "input" >>= print . solve . parse
+
+parse :: String -> Input
+parse = map read . words
+
+toDigits n = reverse (go n)
+ where
+  go n
+    | q == 0 = [r]
+    | otherwise = r : go q
+   where
+    (q, r) = divMod n 10
+
+fromDigits ds = go 0 ds
+ where
+  go acc [] = acc
+  go acc (d : ds) = go (10 * acc + d) ds
+
+solve input = input & iterate step & (!! 25) & length
+ where
+  step [] = []
+  step (0 : ns) = 1 : step ns
+  step (n : ns) =
+    let ds = toDigits n
+        l = length ds
+     in if even l
+          then fromDigits (take (l `div` 2) ds) : fromDigits (drop (l `div` 2) ds) : step ns
+          else 2024 * n : step ns
