4641de7a04bec35ae09e245cdde840da8d7f7997
Paul Brauner <polux2001@gmail.com>
2024-12-18 06:39:41 +0100

    2024 day 18

---
+++ 2024/day18-2/src/Main.hs
@@ -0,0 +1,91 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+
+-- #region language extensions
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+-- #endregion
+
+module Main where
+
+-- #region imports
+
+import Control.Arrow ((>>>),(***), Arrow (first, second))
+import Control.Lens (at, ix, each, folded, isn't, traversed, (%~), (&), (.~), (?~), (^.), (^..), (^?), _1, _2, _3, _4)
+import Control.Lens.Extras (is)
+import qualified Data.Array as A
+import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import Data.Generics.Labels ()
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
+import Debug.Trace (trace, traceShow, traceShowId)
+import GHC.Generics (Generic)
+import Linear (V2 (..), _x, _y)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Pretty.Simple (pPrint, pShow)
+import Text.Regex.PCRE ((=~))
+import Util
+import qualified Data.Graph.Inductive as G
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Safe hiding (at)
+import Data.Functor ((<&>))
+import Data.MemoTrie
+import Data.Maybe (fromJust, catMaybes, isJust)
+import Data.List (sortOn, elemIndex)
+import Data.List.Split (splitOn, chunksOf)
+import Control.Monad (forM_, unless, when)
+
+-- #endregion
+
+type Input = [V2 Int]
+
+main :: IO ()
+main = readFile "input" >>= print . solve . parse
+
+parse :: String -> Input
+parse = map parseLine . lines
+  where
+    parseLine (ints->[x,y]) = V2 x y
+    parseLine _ = error "parsing"
+
+solve input = input !! (index-1)
+ where
+  source = V2 0 0
+  target = V2 70 70
+  gridSize = target + V2 1 1
+
+  index = head [i | (i, grid) <- zip [0..] grids, not (valid grid)]
+
+  grids = scanl (flip S.insert) mempty input
+
+  valid grid = astar neighbors cost isTarget heuristic source & isJust
+    where
+      neighbors = filter (`S.notMember` grid) . neighbors2D gridSize
+      cost _ _ = 1
+      isTarget = (== target)
+      heuristic p = sum (target - p)
