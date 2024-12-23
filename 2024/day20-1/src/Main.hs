87352c7d43d4ab2202680c9cbc61cf0686124130
Paul Brauner <polux2001@gmail.com>
2024-12-23 11:38:40 +0100

    2024 day 20

---
+++ 2024/day20-1/src/Main.hs
@@ -0,0 +1,97 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+-- #region language extensions
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+
+-- #endregion
+
+module Main where
+
+-- #region imports
+
+import Control.Arrow (Arrow (first, second), (***), (>>>))
+import Control.Lens (at, each, folded, isn't, ix, traversed, (%~), (&), (.~), (?~), (^.), (^..), (^?), _1, _2, _3, _4)
+import Control.Lens.Extras (is)
+import Control.Monad (forM_, unless, when)
+import qualified Data.Array as A
+import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import Data.Functor ((<&>))
+import Data.Generics.Labels ()
+import qualified Data.Graph.Inductive as G
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Data.List (elemIndex, sortOn, tails)
+import Data.List.Split (chunksOf, splitOn)
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes, fromJust)
+import Data.MemoTrie
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
+import Debug.Trace (trace, traceShow, traceShowId)
+import GHC.Generics (Generic)
+import Linear (V2 (..), _x, _y)
+import Safe hiding (at)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Pretty.Simple (pPrint, pShow)
+import Text.Regex.PCRE ((=~))
+import Util
+
+-- #endregion
+
+-- #region regex parsing
+{-
+import Text.Regex.Pcre2 (regex)
+
+parseRegex :: String -> [Int]
+parseRegex = map parseLine . T.lines . T.pack
+ where
+   toInt = read . T.unpack
+   parseLine [regex|some example regex: (?<x>\d+)|] = toInt x
+-}
+-- #endregion
+
+type Input = UA.Array (V2 Int) Char
+
+main :: IO ()
+main = readFile "input" >>= print . solve . parse
+
+parse :: String -> Input
+parse = arrayFromList2D . lines
+
+solve input = filter good (pairs distances) & length
+ where
+  n2d = neighbors2D (arraySize input)
+  at p = input UA.! p
+  isWall p = at p == '#'
+
+  start = head [p | (p, c) <- UA.assocs input, c == 'S']
+  end = head [p | (p, c) <- UA.assocs input, c == 'E']
+  distances = bfs (filter (not . isWall) . n2d) (== end) start & fromJust & (`zip` [0 ..])
+
+  pairs xs = [(x,y) | x:xs' <- tails xs, y <- xs']
+
+  good ((i,id), (o,od)) = canReach i o &&  (od-id)-sum (fmap abs (o-i)) >= 100
+  canReach p0 p1 = sum (fmap abs (p1-p0)) <= 2
