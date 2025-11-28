49541158c98a44146278e2efe1e81241ecebb39a
Paul Brauner <polux2001@gmail.com>
2025-11-28 09:00:47 +0100

    pull hls through nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "env";
-  nativeBuildInputs = [ gnumake gcc pkg-config z3 ripgrep difftastic ];
+  nativeBuildInputs = [ gnumake gcc pkg-config z3 ripgrep difftastic haskell.compiler.ghc984 haskell-language-server cabal-install ];
   buildInputs = [ pcre gmp libglvnd libGL libGLU freeglut zlib lazygit ];
   LD_LIBRARY_PATH = with pkgs; "${freeglut}/lib";
 }
