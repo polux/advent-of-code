2301f6083cac70546f2bc13847d6ab5cc1031a34
Paul Brauner <polux2001@gmail.com>
2025-12-11 06:28:06 +0100

    2025 day 11

---
+++ 2025/day11-2/src/Main.hs
@@ -0,0 +1,97 @@
+-- Copyright 2022 Google LLC.
+-- SPDX-License-Identifier: Apache-2.0
+
+-- #region language extensions
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedLabels #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# OPTIONS_GHC -Wno-x-partial #-}
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+-- #endregion
+
+module Main where
+
+-- #region imports
+
+import Control.Arrow ((>>>),(***), Arrow (first, second))
+import Control.Lens (at, ix, each, folded, isn't, traversed, (%~), (&), (.~), (?~), (^.), (^..), (^?), _1, _2, _3, _4)
+import Control.Lens.Extras (is)
+import qualified Data.Array as A
+import qualified Data.Array.Unboxed as UA
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Char8 as BS8
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString.Lazy.Char8 as LBS8
+import Data.Generics.Labels ()
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as M
+import Data.Sequence (Seq (..))
+import qualified Data.Sequence as Seq
+import Data.Set (Set)
+import qualified Data.Set as S
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as LT
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as UV
+import qualified Data.Vector.Unboxed.Mutable as MUV
+import Debug.Trace (trace, traceShow, traceShowId)
+import GHC.Generics (Generic)
+import Linear (V2 (..), _x, _y)
+import qualified Text.Megaparsec as P
+import qualified Text.Megaparsec.Char as P
+import qualified Text.Megaparsec.Char.Lexer as L
+import Text.Pretty.Simple (pPrint, pShow)
+import Text.Regex.PCRE ((=~))
+import Util
+import qualified Data.Graph.Inductive as G
+import Data.Graph.Inductive.PatriciaTree (Gr)
+import Safe hiding (at)
+import Data.Functor ((<&>))
+import Data.MemoTrie
+import Data.Maybe (fromJust, catMaybes)
+import Data.List (sort, sortOn, elemIndex)
+import Data.List.Split (splitOn, chunksOf)
+import Control.Monad (forM_, unless, when)
+
+-- #endregion
+
+-- #region regex parsing
+{-
+import Text.Regex.Pcre2 (regex)
+
+parseRegex :: String -> [Int]
+parseRegex = map parseLine . T.lines . T.pack
+ where
+   toInt = read . T.unpack
+   parseLine [regex|some example regex: (?<x>\d+)|] = toInt x
+-}
+-- #endregion
+
+type Input = Map String [String]
+
+type Output = Input
+
+main :: IO ()
+main = readFile "input" >>= print . solve . parse
+
+parse :: String -> Input
+parse str = M.unionsWith (<>) (map parseLine (lines str))
+  where
+    parseLine (words->((init->v): vs)) = M.singleton v vs
+
+--solve :: Input -> Output
+solve input = mgo False False "svr"
+  where
+    mgo = memo3 go
+
+    go :: Bool -> Bool -> String -> Int
+    go fft dac "out" = if fft && dac then 1 else 0
+    go fft dac v = sum [mgo (fft || v == "fft") (dac || v == "dac") v' | v' <- input M.! v]
